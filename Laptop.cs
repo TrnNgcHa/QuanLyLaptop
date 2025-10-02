e21c0c46406d7c6db0d81f2ba1e47e7e56a2a02f
TrnNgcHa <ctythanhhien1974@gmail.com>
2025-10-02 21:43:31 +0700

    Add files via upload

--- Laptop.cs
+++ Laptop.cs
@@ -8,6 +8,12 @@ namespace QuanLyLaptop
 {
     internal class Laptop
     {
-        
+        public int MaLaptop { get; set; }
+        public string TenLaptop { get; set; }
+        public string HangSanXuat { get; set; }
+        public string NgayNhap { get; set; }
+        public int SoTon { get; set; }
+        public int SoLuong { get; set; }
+        public string GiaLaptop { get; set; }
     }
 }
