df34a2ec70f3042a88aacd35207388facbd1ba27
TrnNgcHa <ctythanhhien1974@gmail.com>
2025-10-19 08:02:27 +0700

    up

--- LaptopList.cs
+++ LaptopList.cs
@@ -22,32 +22,84 @@ namespace QuanLyLaptop
 
         private void DanhSachLaptop_Load(object sender, EventArgs e)
         {
-            
+            dgvDanhSachLaptop.DataSource = MainMenu.Laptops;
+            lblTenNguoiDung.Text = Authentication.CurrentAccount.FirstName + " " + Authentication.CurrentAccount.LastName;
+            lblTenTaiKhoan.Text = Authentication.CurrentAccount.AccountName;
+            lblSoDu.Text = string.Format("{0:#,##0 VND}", Authentication.CurrentAccount.Balance);
+
+            dgvDanhSachLaptop.Columns["GiaTien"].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+            dgvDanhSachLaptop.Columns["GiaTien"].DefaultCellStyle.Format = "#,##0 VND";
+
+
         }
 
         private void dgvDanhSachLaptop_DataBindingComplete(object sender, DataGridViewBindingCompleteEventArgs e)
         {
-            
+            foreach (DataGridViewRow row in dgvDanhSachLaptop.Rows)
+            {
+                if (!row.IsNewRow)
+                    row.Cells["STT1"].Value = row.Index + 1;
+            }
         }
 
         private void dgvDanhSachLaptop_CellEnter(object sender, DataGridViewCellEventArgs e)
         {
-            
+            if (dgvDanhSachLaptop.CurrentRow != null)
+            {
+                lblTenLaptop.Text = dgvDanhSachLaptop.CurrentRow.Cells["TenLaptop"].Value.ToString();
+                lblCPUInfo.Text = dgvDanhSachLaptop.CurrentRow.Cells["CPU"].Value.ToString();
+                lblGPUInfo.Text = dgvDanhSachLaptop.CurrentRow.Cells["GPU"].Value.ToString();
+                lblRAMInfo.Text = dgvDanhSachLaptop.CurrentRow.Cells["RAM"].Value.ToString();
+                lblStorageInfo.Text = dgvDanhSachLaptop.CurrentRow.Cells["OCung"].Value.ToString();
+            }
         }
 
         private void btnReview_Click(object sender, EventArgs e)
         {
-           
+            var form = new Review();
+            form.Show();
         }
 
         private void btnPurchase_Click(object sender, EventArgs e)
         {
-            
+            Laptop SelectedItem = dgvDanhSachLaptop.CurrentRow?.DataBoundItem as Laptop;
+            var form = new Receipt(SelectedItem);
+            form.TopMost = true;
+            form.Show();
+        }
+
+        private void lblTenLaptop_TextChanged(object sender, EventArgs e)
+        {
+
+
         }
 
         private void btnTimKiem_Click(object sender, EventArgs e)
         {
-            
+            string filterText = txtFilter.Text.ToLower();
+            if (string.IsNullOrEmpty(filterText))
+            {
+                dgvDanhSachLaptop.DataSource = MainMenu.Laptops; // danh sách thời điểm frmStudentView_Load
+            }
+            else
+            {
+                List<Laptop> filtered = new List<Laptop>();
+                switch (cmbLoaiTimKiem.Text)
+                {
+                    case "Mã Laptop":
+                        filtered = MainMenu.Laptops.Where(s => s.LaptopID.ToString().ToLower().Contains(filterText)).ToList();
+                        break;
+
+                    case "Tên Laptop":
+                        filtered = MainMenu.Laptops.Where(s => s.LaptopName.ToLower().Contains(filterText)).ToList();
+                        break;
+                    case "Hãng Laptop":
+                        filtered = MainMenu.Laptops.Where(s => s.AgencyName.ToLower().Contains(filterText)).ToList();
+                        break;
+
+                }
+                dgvDanhSachLaptop.DataSource = filtered;
+            }
         }
     }
 }
